4fecf4fb738b9aee67c632229d708d340a97addb
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-14 18:24:51 -0600

    Create a new controller to get the tables information

---
+++ TrackerBar-Admin.API/Controllers/TableController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrackerBar_Admin.API.Repositories;
+
+namespace TrackerBar_Admin.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableController : ControllerBase
+    {
+        private readonly ITableRepository tableRepository;
+
+        public TableController(ITableRepository tableRepository)
+        { 
+            this.tableRepository = tableRepository;
+        }
+
+        [HttpGet]
+        [Route("total")]
+        public async Task<IActionResult> GetTotalTables()
+        {
+            var qty = await tableRepository.GetTotalTablesAsync();
+            return Ok(qty);
+        }
+    }
+}
