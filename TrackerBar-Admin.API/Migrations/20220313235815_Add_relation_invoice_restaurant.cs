6f2605c7090e05c2292b81401d47d756edb8e051
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-03-14 18:25:28 -0600

    Commits of the DB migrations

---
+++ TrackerBar-Admin.API/Migrations/20220313235815_Add_relation_invoice_restaurant.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackerBar_Admin.API.Migrations
+{
+    public partial class Add_relation_invoice_restaurant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantId",
+                table: "ReceiptDetail",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptDetail_RestaurantId",
+                table: "ReceiptDetail",
+                column: "RestaurantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReceiptDetail_Restaurant_RestaurantId",
+                table: "ReceiptDetail",
+                column: "RestaurantId",
+                principalTable: "Restaurant",
+                principalColumn: "RestaurantId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReceiptDetail_Restaurant_RestaurantId",
+                table: "ReceiptDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ReceiptDetail_RestaurantId",
+                table: "ReceiptDetail");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "ReceiptDetail");
+        }
+    }
+}
---
+++ TrackerBar-Admin.API/Migrations/20220314000029_Add_relation_invoice_restaurant_2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackerBar_Admin.API.Migrations
+{
+    public partial class Add_relation_invoice_restaurant_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
