bec728a7eb7a13cf8815d59894e11da7eb37a536
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-24 10:49:02 -0600

    Create a new model for reservations response

---
+++ TrackerBar-Admin.API/DataModels/Reservations.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackerBar_Admin.API.DataModels
+{
+    public class Reservations
+    {
+        public int TableNumber { get; set; }
+        public int PeopleQty{ get; set; }
+        public string Name { get; set; }
+        public string Last { get; set; }
+        public DateTime boughtAt { get; set; }
+
+    }
+}
